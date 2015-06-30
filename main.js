ddf710a6d7113f70b54ff3aa771461f4271b992c
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-30 13:15:54 -0400

    Added "Instructions"

---
+++ main.js
@@ -0,0 +1,12 @@
+var wholePage = document.getElementById("wholePage");
+var instructions =
+"\
+<h3>Instructions:</h3>\
+<br>\
+<p>Insert some instructions here...</p>\
+<br>\
+<p>Insert some more instructions here...</p>\
+<br>\
+<p>You get the idea...</p>\
+";
+wholePage.innerHTML = instructions;
