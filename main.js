d0d5512460cdcb51e619695d0618984504159690
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-30 13:27:20 -0400

    Started mechanism to leave the instructions and get started.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Set up the first part of the page, with instructions and such
 var wholePage = document.getElementById("wholePage");
 var instructions =
 "\
@@ -14,3 +15,8 @@ var instructions =
 <div id=\"continue\"><p>Are you ready to continue?</p></div>\
 ";
 wholePage.innerHTML = instructions;
+
+//Set up the beginning of the flowchart
+document.getElementById("continue").addEventListener("click", function() {
+  
+});
