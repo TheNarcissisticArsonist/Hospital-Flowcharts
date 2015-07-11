e07d1983fd59231f200b244087ea759659aa9411
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-11 15:47:09 -0400

    Removed dummy elements from path div.

--- main.js
+++ main.js
@@ -30,10 +30,7 @@ var mainPageFormat = "\
 <img id=\"flowchart\" src=\"\"></img>\
 <br>\
 <br>\
-<div id=\"path\">\
-  <div class=\"chosenItem\"><p>First Stop</p></div>\
-  <div class=\"chosenItem\"><p>Second Stop</p></div>\
-</div>\
+<div id=\"path\"></div>\
 "
 document.getElementById("continue").addEventListener("click", function() {
   wholePage.innerHTML = mainPageFormat;
@@ -70,13 +67,17 @@ function setUpBeginning() {
 //The slide class
 function slide(choices, colors, image, slideTitle, slideInstructions) {
   this.choices = choices;
+  //Add p tags around each item in choices[]
   for(i=0; i<4; i++) {
     this.choices[i] = "<p>" + this.choices[i] + "</p>";
   }
+
   this.colors = colors;
   this.image = image;
   this.slideTitle = slideTitle;
   this.slideInstructions = slideInstructions;
+
+  //This loads the slide onto the page
   this.load = function() {
     for(i=0; i<4; i++) {
       elements.choices[i].innerHTML = this.choices[i];
